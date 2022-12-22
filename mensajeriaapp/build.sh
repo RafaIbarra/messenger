ee5aa73ea7af4c86b0f695e1b72dfda779f7be5a
RafaIbarra <blasrafael1986@gmail.com>
2022-12-22 17:14:01 -0300

    agregando pip

--- mensajeriaapp/build.sh
+++ mensajeriaapp/build.sh
@@ -3,10 +3,11 @@
 set -o errexit
 
 poetry install
+poetry add gunicorn
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-daphne -p 8001 mensajeriaapp.asgi:application
-daphne mensajeriaapp.asgi:application
+daphne -p 8001 ./mensajeriaapp.asgi:application
+daphne ./mensajeriaapp.asgi:application
