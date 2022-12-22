3b4eb8ee34405f69b81c4fdd28694b3fc1460706
RafaIbarra <blasrafael1986@gmail.com>
2022-12-21 22:45:57 -0300

    modificacion de configuracion channels nueva

--- mensajeriaapp/mensajeriaapp/settings.py
+++ mensajeriaapp/mensajeriaapp/settings.py
@@ -175,7 +175,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis-server-name", 6379)],
+             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
