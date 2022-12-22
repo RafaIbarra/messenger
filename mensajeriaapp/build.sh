62d4f1e46b33b86abb2169305399a7190948ca80
RafaIbarra <blasrafael1986@gmail.com>
2022-12-21 23:42:32 -0300

    agregar importlib

--- mensajeriaapp/build.sh
+++ mensajeriaapp/build.sh
@@ -2,8 +2,11 @@
 # exit on error
 set -o errexit
 
-# poetry install
+poetry install
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+daphne -p 8001 mensajeriaapp.asgi:application
+daphne mensajeriaapp.asgi:application
--- mensajeriaapp/mensajeriaapp/wsgi.py
+++ mensajeriaapp/mensajeriaapp/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mensajeriaapp.settings')
 
 application = get_wsgi_application()
+
+import importlib
+if __name__ == '__main__':
+    print("In main.py")
+    package = importlib.import_module('hello', package='./')
+    print("Loaded hello.py")
+    package.hello()
